676a20d636b68ca50df127a17f1a250d9e44362b
infloop <infloop.gm@gmail.com>
2022-11-10 18:01:05 +0300

    fix: lint cleanup

--- src/migrations/Migration20221108113654.ts
+++ src/migrations/Migration20221108113654.ts
@@ -2,10 +2,12 @@ import { Migration } from '@mikro-orm/migrations';
 
 export class Migration20221108113654 extends Migration {
   async up() {
-    this.addSql('create table "test" (' +
+    this.addSql(
+      'create table "test" (' +
         '"id" int not null, ' +
         '"value" varchar(255) not null, ' +
-        'constraint "test_pkey2" primary key ("id"));');
+        'constraint "test_pkey2" primary key ("id"));',
+    );
   }
 
   async down(): Promise<void> {
--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires  */
 import { Options, Utils } from '@mikro-orm/core';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 import * as glob from 'glob';
 import * as path from 'path';
 import { MigrationObject } from '@mikro-orm/core/typings';
@@ -8,60 +9,64 @@ dotenv.config();
 
 // TODO move this to nestjs packages
 const findMigrations = (mainFolder: string, npmPackageNames: string[]): MigrationObject[] => {
-    const cwd = process.cwd();
+  const cwd = process.cwd();
 
-    const folders = [mainFolder, ...npmPackageNames.map(npmPackage => `./node_modules/${npmPackage}/dist/migrations/`)];
-    const filenames = folders.map(folder => {
-        const extPattern = Utils.detectTsNode() ? '!(*.d).{js,ts}' : '*.js';
-        const filePathPattern = `${path.isAbsolute(folder) ? folder : path.join(cwd, folder)}/Migration${extPattern}`;
-        return glob.sync(filePathPattern);
-    }).flat();
+  const folders = [mainFolder, ...npmPackageNames.map((npmPackage) => `./node_modules/${npmPackage}/dist/migrations/`)];
+  const filenames = folders
+    .map((folder) => {
+      const extPattern = Utils.detectTsNode() ? '!(*.d).{js,ts}' : '*.js';
+      const filePathPattern = `${path.isAbsolute(folder) ? folder : path.join(cwd, folder)}/Migration${extPattern}`;
+      return glob.sync(filePathPattern);
+    })
+    .flat();
 
-    const migrations = filenames.map(filename => {
-        const module = require(filename);
-        const ext = path.extname(filename, );
-        const fileNameWithoutExt = path.basename(filename, ext);
+  const migrations = filenames
+    .map((filename) => {
+      const module = require(filename);
+      const ext = path.extname(filename);
+      const fileNameWithoutExt = path.basename(filename, ext);
 
-        const clazz = module[fileNameWithoutExt];
+      const clazz = module[fileNameWithoutExt];
 
-        if (clazz) {
-            return { name: fileNameWithoutExt, class: clazz };
-        }
+      if (clazz) {
+        return { name: fileNameWithoutExt, class: clazz };
+      }
 
-        return null;
-    }).filter(i => i);
+      return null;
+    })
+    .filter((i) => i);
 
-    // TODO think about Nest.js logger
-    console.log(`Found [${migrations.length}] DB migration files.`);
+  // TODO think about Nest.js logger
+  console.log(`Found [${migrations.length}] DB migration files.`);
 
-    return migrations;
-}
+  return migrations;
+};
 
 // TODO move this to nestjs packages
 const getMigrationOptions = (mainMigrationsFolder: string, npmPackageNames: string[]): Options['migrations'] => {
-    return {
-        tableName: 'mikro_orm_migrations', // name of database table with log of executed transactions
-        path: mainMigrationsFolder, // path to the folder with migrations
-        transactional: true, // wrap each migration in a transaction
-        disableForeignKeys: true, // wrap statements with `set foreign_key_checks = 0` or equivalent
-        allOrNothing: true, // wrap all migrations in master transaction
-        dropTables: true, // allow to disable table dropping
-        safe: false, // allow to disable table and column dropping
-        snapshot: false, // save snapshot when creating new migrations
-        emit: 'ts', // migration generation mode,
-        migrationsList: findMigrations(mainMigrationsFolder, npmPackageNames),
-    };
+  return {
+    tableName: 'mikro_orm_migrations', // name of database table with log of executed transactions
+    path: mainMigrationsFolder, // path to the folder with migrations
+    transactional: true, // wrap each migration in a transaction
+    disableForeignKeys: true, // wrap statements with `set foreign_key_checks = 0` or equivalent
+    allOrNothing: true, // wrap all migrations in master transaction
+    dropTables: true, // allow to disable table dropping
+    safe: false, // allow to disable table and column dropping
+    snapshot: false, // save snapshot when creating new migrations
+    emit: 'ts', // migration generation mode,
+    migrationsList: findMigrations(mainMigrationsFolder, npmPackageNames),
+  };
 };
 
 const config: Options = {
-    type: 'postgresql',
-    dbName: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT, 10),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    entities: [RegistryKey, RegistryOperator, RegistryMeta],
-    migrations: getMigrationOptions(path.join(__dirname, 'migrations'), ['@lido-nestjs/registry']),
+  type: 'postgresql',
+  dbName: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  entities: [RegistryKey, RegistryOperator, RegistryMeta],
+  migrations: getMigrationOptions(path.join(__dirname, 'migrations'), ['@lido-nestjs/registry']),
 };
 
 export default config;
