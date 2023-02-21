6fe2bcbce5ca1c92e8da9487f2fa5753dd5037c6
Anna Mukharram <a.mukharram@fun-box.ru>
2023-02-21 13:42:11 +0400

    fix: metrics

--- src/jobs/registry/registry.service.ts
+++ src/jobs/registry/registry.service.ts
@@ -51,7 +51,10 @@ export class RegistryService {
   private async updateKeys(): Promise<void> {
     await this.jobService.wrapJob({ name: 'Update keys from NodeOperatorRegistry' }, async () => {
       await this.keyRegistryService.update('latest');
-      await this.updateMetaForMetrics();
+
+      // Update cached data to quick access
+      await Promise.all([this.updateMetaForMetrics(), this.updateOperatorsMap()]);
+
       this.updateMetrics();
     });
   }
@@ -59,6 +62,7 @@ export class RegistryService {
   protected lastTimestamp: number | undefined = undefined;
   protected lastBlockNumber: number | undefined = undefined;
   protected lastNonce: number | undefined = undefined;
+  protected operatorsMap: Record<number, RegistryOperator> = {};
 
   /**
    * Updates timestamp of the last registry update
@@ -68,6 +72,18 @@ export class RegistryService {
     this.lastTimestamp = meta?.timestamp ?? this.lastTimestamp;
     this.lastBlockNumber = meta?.blockNumber ?? this.lastBlockNumber;
     this.lastNonce = meta?.keysOpIndex ?? this.lastNonce;
+  }
+
+  /**
+   * Updates cached operators map
+   */
+  protected async updateOperatorsMap(): Promise<void> {
+    const operators = await this.operatorStorageService.findAll();
+
+    this.operatorsMap = operators.reduce((operatorsMap, operator) => {
+      operatorsMap[operator.index] = operator;
+      return operatorsMap;
+    }, {});
   }
 
   public async getKeyWithMetaByPubkey(pubkey: string): Promise<{ keys: RegistryKey[]; meta: RegistryMeta | null }> {
@@ -169,6 +185,30 @@ export class RegistryService {
     this.lastBlockNumber && this.prometheusService.registryBlockNumber.set(this.lastBlockNumber);
     // this value will be different for all SR modules
     this.lastNonce && this.prometheusService.registryNonce.set({ srModuleId: 1 }, this.lastNonce);
+
+    const operators = Object.values(this.operatorsMap);
+
+    this.prometheusService.registryNumberOfKeysBySRModuleAndOperator.reset();
+
+    operators.forEach((operator) => {
+      this.prometheusService.registryNumberOfKeysBySRModuleAndOperator.set(
+        {
+          operator: operator.index,
+          srModuleId: 1,
+          used: 1,
+        },
+        operator.usedSigningKeys,
+      );
+
+      this.prometheusService.registryNumberOfKeysBySRModuleAndOperator.set(
+        {
+          operator: operator.index,
+          srModuleId: 1,
+          used: 0,
+        },
+        operator.totalSigningKeys - operator.usedSigningKeys,
+      );
+    });
 
     this.logger.log('Registry metrics updated');
   }
