3304d16584694a57a8a05f449dd71e170650463f
Anna Mukharram <a.mukharram@fun-box.ru>
2023-03-31 16:01:10 +0400

    fix: reading type from SR module

--- src/common/contracts/staking-router/staking-router-fetch.service.ts
+++ src/common/contracts/staking-router/staking-router-fetch.service.ts
@@ -43,27 +43,16 @@ export class StakingRouterFetchService {
     const transformedModules = await Promise.all(
       modules.map(async (stakingModule) => {
         // const isActive = await contract.getStakingModuleIsActive(stakingModule.id, { blockTag } as any);
-        // until the end of voting work with old version of Node Operator Registry
-        // this version doesnt correspond to IStakingModule interface
-        // currently skip this section
+        const stakingModuleType = (await this.iStakingModule.getType(
+          stakingModule.stakingModuleAddress,
+          blockTag,
+        )) as STAKING_MODULE_TYPE;
 
-        // const stakingModuleType = (await this.iStakingModule.getType(
-        //   stakingModule.stakingModuleAddress,
-        //   blockTag,
-        // )) as STAKING_MODULE_TYPE;
-
-        // if (!Object.values(STAKING_MODULE_TYPE).includes(stakingModuleType)) {
-        //   throw new Error(`Staking Module type ${stakingModuleType} is unknown`);
-        // }
-
-        // lets put type by id as we know that list contains only NOR contract
-
-        if (stakingModule.id != 1) {
-          this.logger.error(new Error(`Staking Module id ${stakingModule.id} is unknown`));
-          process.exit(1);
+        if (!Object.values(STAKING_MODULE_TYPE).includes(stakingModuleType)) {
+          throw new Error(`Staking Module type ${stakingModuleType} is unknown`);
         }
 
-        const stakingModuleType = STAKING_MODULE_TYPE.CURATED_ONCHAIN_V1_TYPE;
+        console.log('Staking module type', stakingModuleType);
 
         return {
           id: stakingModule.id,
