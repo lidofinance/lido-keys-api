9ffda649ac1828a2db1bb6c5d728be53fb209ff9
Eddort <webisforce@gmail.com>
2023-08-28 20:41:40 +0400

    wip: update nse

--- src/app/simple-dvt.e2e-chain.ts
+++ src/app/simple-dvt.e2e-chain.ts
@@ -1,20 +1,16 @@
-import { createSDK, SDK, StoryResult } from 'nse-test';
+import { createSDK, nse } from 'nse-test';
 
 jest.setTimeout(100_000);
 
 describe('Simple DVT', () => {
-  // TODO: move to nse
-  let sdk: SDK;
-  // TODO: get story type
-  // let initialState: StoryResult<'std/accounts'>;
-  let initialState: any;
+  let sdk: nse.SDK;
+  let session: nse.Session;
+  let initialState: nse.StoryResult<'simple-dvt-mock/initial-state'>;
   beforeAll(async () => {
     sdk = await createSDK('http://localhost:8001');
 
-    const { port } = await sdk.env.hardhat({});
-    initialState = await sdk.story('simple-dvt-mock/initial-state', {
-      sessionKey: port,
-    });
+    session = await sdk.env.hardhat({});
+    initialState = await session.story('simple-dvt-mock/initial-state', {});
   });
 
   test('initial state created', () => {
