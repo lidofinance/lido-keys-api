656b375944297e33372f46582baad4859f174e38
Eddort <webisforce@gmail.com>
2023-08-31 10:52:02 +0400

    refactor: rewrite kapi nse tests

--- src/app/simple-dvt.e2e-chain.ts
+++ src/app/simple-dvt.e2e-chain.ts
@@ -1,34 +1,18 @@
-import {
-  BatchProviderModule,
-  ExtendedJsonRpcBatchProvider,
-  SimpleFallbackJsonRpcBatchProvider,
-} from '@lido-nestjs/execution';
-import { LoggerModule, nullTransport } from '@lido-nestjs/logger';
+import { SimpleFallbackJsonRpcBatchProvider } from '@lido-nestjs/execution';
 import { MikroORM } from '@mikro-orm/core';
-import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { INestApplication, ValidationPipe, VersioningType } from '@nestjs/common';
-import { Test } from '@nestjs/testing';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { Test, TestingModule } from '@nestjs/testing';
 
 import { createSDK, nse } from 'nse-test';
 
-import { RegistryKeyStorageService, RegistryStorageService } from '../common/registry';
-import { KeysController, KeysService } from '../http/keys';
-
-import { StakingRouterModule } from '../staking-router-modules/staking-router.module';
-import { StakingRouterFetchService } from '../staking-router-modules/contracts';
+import { RegistryKeyStorageService } from '../common/registry';
 import { ElMetaStorageService } from '../storage/el-meta.storage';
 import { SRModuleStorageService } from '../storage/sr-module.storage';
 import { KeysUpdateService } from '../jobs/keys-update';
-import { ScheduleModule } from '@nestjs/schedule';
 import { ExecutionProvider, ExecutionProviderService } from '../common/execution-provider';
 import { ConfigService } from '../common/config';
 import { PrometheusService } from '../common/prometheus';
-import { JobService } from '../common/job';
-import { StakingModuleInterfaceService } from '../staking-router-modules/contracts/staking-module-interface';
-import { LidoLocatorService } from '../staking-router-modules/contracts/lido-locator';
-import { LidoLocator__factory, LIDO_LOCATOR_CONTRACT_TOKEN } from '@lido-nestjs/contracts';
-import { KeyWithModuleAddress } from '../http/keys/entities';
+import { StakingRouterService } from '../staking-router-modules/staking-router.service';
+import { AppModule } from './app-testing.module';
 
 jest.setTimeout(100_000);
 
@@ -37,115 +21,64 @@ describe('Simple DVT', () => {
   let session: nse.HardhatSession;
   let initialState: nse.StoryResult<'simple-dvt-mock/initial-state'>;
 
-  let prevBlockNumber = 0;
-
-  let app: INestApplication;
+  let moduleRef: TestingModule;
 
   let keysStorageService: RegistryKeyStorageService;
   let moduleStorageService: SRModuleStorageService;
   let elMetaStorageService: ElMetaStorageService;
-  let registryStorage: RegistryStorageService;
 
-  let keysService: KeysService;
+  let stakingRouterService: StakingRouterService;
   let keysUpdateService: KeysUpdateService;
+
+  let prevBlockNumber = 0;
+
   beforeAll(async () => {
     sdk = await createSDK('http://localhost:8001');
 
     session = await sdk.env.hardhat({});
     initialState = await session.story('simple-dvt-mock/initial-state', {});
 
-    const providerModule = BatchProviderModule.forRoot({ url: `http://127.0.0.1:${session.env.port}` });
-    const imports = [
-      //  sqlite3 only supports serializable transactions, ignoring the isolation level param
-      // TODO: use postgres
-      MikroOrmModule.forRoot({
-        dbName: ':memory:',
-        type: 'sqlite',
-        allowGlobalContext: true,
-        entities: ['./**/*.entity.ts'],
-      }),
-      LoggerModule.forRoot({ transports: [nullTransport()] }),
-      ScheduleModule.forRoot(),
-      providerModule,
-      StakingRouterModule.forFeatureAsync({
-        inject: [ExtendedJsonRpcBatchProvider],
-        async useFactory(provider) {
-          return { provider };
+    moduleRef = await Test.createTestingModule({ imports: [AppModule] })
+      .overrideProvider(ExecutionProviderService)
+      .useValue(session.provider)
+      .overrideProvider(SimpleFallbackJsonRpcBatchProvider)
+      .useValue(session.provider)
+      .overrideProvider(ExecutionProvider)
+      .useValue(session.provider)
+      .overrideProvider(PrometheusService)
+      .useValue({
+        httpRequestDuration: jest.fn(),
+        buildInfo: jest.fn(),
+        elRpcRequestDuration: jest.fn(),
+        clApiRequestDuration: jest.fn(),
+        jobDuration: jest.fn(),
+        registryLastUpdate: jest.fn(),
+        validatorsRegistryLastTimestampUpdate: jest.fn(),
+        registryNumberOfKeysBySRModuleAndOperator: jest.fn(),
+        registryNonce: jest.fn(),
+        registryBlockNumber: jest.fn(),
+        validatorsRegistryLastBlockNumber: jest.fn(),
+        validatorsRegistryLastSlot: jest.fn(),
+        validatorsEnabled: jest.fn(),
+      })
+      .overrideProvider(ConfigService)
+      .useValue({
+        get(path) {
+          const conf = { LIDO_LOCATOR_ADDRESS: initialState.locatorAddress };
+          return conf[path];
         },
-      }),
-    ];
-
-    const controllers = [KeysController];
-    const providers = [
-      // { provide: StakingRouterFetchService, useFactory: StakingRouterFetchService },
-      { provide: ExecutionProviderService, useValue: session.provider },
-      { provide: SimpleFallbackJsonRpcBatchProvider, useValue: session.provider },
-      { provide: ExecutionProvider, useValue: session.provider },
-      {
-        provide: LIDO_LOCATOR_CONTRACT_TOKEN,
-        useValue: LidoLocator__factory.connect(initialState.locatorAddress, session.provider),
-      },
-      {
-        provide: ConfigService,
-        useValue: {
-          get(path) {
-            const conf = { LIDO_LOCATOR_ADDRESS: initialState.locatorAddress };
-            return conf[path];
-          },
-        },
-      },
-      {
-        provide: JobService,
-        useValue: { wrapJob: async (_, cb) => await cb() },
-      },
-      {
-        provide: PrometheusService,
-        useValue: {
-          httpRequestDuration: jest.fn(),
-          buildInfo: jest.fn(),
-          elRpcRequestDuration: jest.fn(),
-          clApiRequestDuration: jest.fn(),
-          jobDuration: jest.fn(),
-          registryLastUpdate: jest.fn(),
-          validatorsRegistryLastTimestampUpdate: jest.fn(),
-          registryNumberOfKeysBySRModuleAndOperator: jest.fn(),
-          registryNonce: jest.fn(),
-          registryBlockNumber: jest.fn(),
-          validatorsRegistryLastBlockNumber: jest.fn(),
-          validatorsRegistryLastSlot: jest.fn(),
-          validatorsEnabled: jest.fn(),
-        },
-      },
-      KeysService,
-      KeysUpdateService,
-      StakingRouterFetchService,
-      StakingModuleInterfaceService,
-      LidoLocatorService,
-    ];
-
-    const moduleRef = await Test.createTestingModule({ imports, controllers, providers })
-      // .overrideProvider(ExecutionProviderModule)
-      // .useValue(providerModule)
+      })
       .compile();
-
-    elMetaStorageService = moduleRef.get(ElMetaStorageService);
-    keysStorageService = moduleRef.get(RegistryKeyStorageService);
-    moduleStorageService = moduleRef.get(SRModuleStorageService);
-    keysService = moduleRef.get(KeysService);
-    registryStorage = moduleRef.get(RegistryStorageService);
-    keysUpdateService = moduleRef.get(KeysUpdateService);
-
-    // disable cron scheduling
-    jest.spyOn(keysUpdateService, 'initialize').mockImplementation(() => Promise.resolve());
 
     const generator = moduleRef.get(MikroORM).getSchemaGenerator();
     await generator.updateSchema();
 
-    app = moduleRef.createNestApplication<NestFastifyApplication>(new FastifyAdapter());
-    app.enableVersioning({ type: VersioningType.URI });
-    app.useGlobalPipes(new ValidationPipe({ transform: true }));
+    elMetaStorageService = moduleRef.get(ElMetaStorageService);
+    keysStorageService = moduleRef.get(RegistryKeyStorageService);
+    moduleStorageService = moduleRef.get(SRModuleStorageService);
 
-    await app.init();
+    keysUpdateService = moduleRef.get(KeysUpdateService);
+    stakingRouterService = moduleRef.get(StakingRouterService);
   });
 
   afterAll(async () => {
@@ -153,9 +86,7 @@ describe('Simple DVT', () => {
     await moduleStorageService.removeAll();
     await elMetaStorageService.removeAll();
 
-    await registryStorage.onModuleDestroy();
-    await app.getHttpAdapter().close();
-    await app.close();
+    await moduleRef.close();
   });
 
   test('initial state created', async () => {
@@ -166,27 +97,29 @@ describe('Simple DVT', () => {
 
   test('update keys api keys', async () => {
     await keysUpdateService.update();
-    const {
-      meta: { elBlockSnapshot },
-      keysGenerators,
-    } = await keysService.get({});
-    const currentBlockNumber = (await session.provider.getBlock('latest')).number;
 
-    expect(elBlockSnapshot.blockNumber).toBe(currentBlockNumber);
+    const currentBlockNumber = (await session.provider.getBlock('latest')).number;
+    const elSnapshot = await stakingRouterService.getElBlockSnapshot();
+
+    expect(elSnapshot).toBeDefined();
+    expect(elSnapshot?.blockNumber).toBe(currentBlockNumber);
     prevBlockNumber = currentBlockNumber;
 
-    const keys: KeyWithModuleAddress[] = [];
-    for (const keysGenerator of keysGenerators) {
-      for await (const keysBatch of keysGenerator) {
-        keys.push(keysBatch);
-      }
-    }
-    const { simpleDVTModuleState, curatedModuleState } = initialState;
+    const srModules = await stakingRouterService.getStakingModules();
 
-    const simpleDVTKeysCount = simpleDVTModuleState.keysCount * simpleDVTModuleState.nodeOperatorsCount;
-    const curatedModuleKeysCount = curatedModuleState.keysCount * curatedModuleState.nodeOperatorsCount;
-    // const keys = await Promise.all([...keysGenerators.flat()]);
-    expect(keys).toHaveLength(simpleDVTKeysCount + curatedModuleKeysCount);
+    expect(srModules).toHaveLength(2);
+
+    const initialModules = [initialState.curatedModuleState, initialState.simpleDVTModuleState];
+    for (const [index, srModule] of srModules.entries()) {
+      const moduleInstance = stakingRouterService.getStakingRouterModuleImpl(srModule.type);
+      const keys = await moduleInstance.getKeys(srModule.stakingModuleAddress, {});
+      const operators = await moduleInstance.getOperators(srModule.stakingModuleAddress);
+
+      const onchainModuleState = initialModules[index];
+
+      expect(operators).toHaveLength(onchainModuleState.nodeOperatorsCount);
+      expect(keys).toHaveLength(onchainModuleState.keysCount * onchainModuleState.nodeOperatorsCount);
+    }
   });
 
   test('meta is updating correctly', async () => {
@@ -198,12 +131,10 @@ describe('Simple DVT', () => {
 
     await keysUpdateService.update();
 
-    const {
-      meta: { elBlockSnapshot },
-    } = await keysService.get({});
+    const elSnapshot = await stakingRouterService.getElBlockSnapshot();
 
-    expect(elBlockSnapshot.blockNumber).toBe(currentBlockNumber);
-
+    expect(elSnapshot).toBeDefined();
+    expect(elSnapshot?.blockNumber).toBe(currentBlockNumber);
     prevBlockNumber = currentBlockNumber;
   });
 });
