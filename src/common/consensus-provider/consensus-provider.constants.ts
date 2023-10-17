9c431f9def83807c9a68a6d1e35924ef4c733230
Anna Mukharram <amuhar3@gmail.com>
2023-10-17 12:59:49 +0400

    fix: comment

--- src/common/consensus-provider/consensus-provider.constants.ts
+++ src/common/consensus-provider/consensus-provider.constants.ts
@@ -2,5 +2,5 @@ export const CONSENSUS_POOL_INTERVAL_MS = 10_000;
 
 export const CONSENSUS_RETRY_DELAY = 1_000;
 export const CONSENSUS_RETRY_ATTEMPTS = 3;
-// we need such a big timeout because we
+// Require a substantial timeout period due to the large volume of data being streamed from the consensus layer and subsequently written into the database
 export const CONSENSUS_REQUEST_TIMEOUT = 1_320_000;
