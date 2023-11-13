f89211ac7f8fe42b34df5cd6af59c4c4eb09fff8
infloop <infloop.gm@gmail.com>
2023-11-13 13:48:12 +0500

    fix: add jsdoc fix for splitHex function

--- src/common/registry/fetch/utils/split-hex.ts
+++ src/common/registry/fetch/utils/split-hex.ts
@@ -1,14 +1,14 @@
 /**
- * Split one big string into array of strings
+ * Split one big hex-like string into array of hex-like strings
  * `0x${key1}{key2}...` -> `[`0x${key1}`, `0x${key2}`]`
  *
  * example record:
  * 0x81b4ae61a898396903897f94bea0e062c3a6925ee93d30f4d4aee93b533b49551ac337da78ff2ab0cfbb0adb380cad94953805708367b0b5f6710d41608ccdd0d5a67938e10e68dd010890d4bfefdcde874370423b0af0d0a053b7b98ae2d6ed
  *
  * 0x81b4ae61a898396903897f94bea0e062c3a6925ee93d30f4d4aee93b533b49551ac337da78ff2ab0cfbb0adb380cad94
- * @param record pubkey or signature merged string
- * @param capacity 96 or 192
- * @returns array of keys or signatures
+ * @param hexString hex-like string
+ * @param chunkLength chunk length
+ * @returns array of hex-like strings
  */
 export const splitHex = (hexString: string, chunkLength: number) => {
   if (chunkLength < 1) {
