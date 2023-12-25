1dc39ecb2723c8d3e7bf5266837cb6eae7fc1cb0
Eddort <webisforce@gmail.com>
2023-12-25 13:55:37 +0100

    fix: wrong migration

--- src/migrations/Migration20231225124800.ts
+++ src/migrations/Migration20231225124800.ts
@@ -1,7 +1,12 @@
 import { Migration } from '@mikro-orm/migrations';
 
-export class Migration20231220104403 extends Migration {
+export class Migration20231225124800 extends Migration {
   async up(): Promise<void> {
+    this.addSql('TRUNCATE registry_key');
+    this.addSql('TRUNCATE registry_operator');
+    this.addSql('TRUNCATE el_meta_entity');
+    this.addSql('TRUNCATE sr_module_entity');
+
     this.addSql('alter table "el_meta_entity" add column "last_changed_block_hash" varchar(66) not null;');
 
     this.addSql('alter table "registry_operator" add column "finalized_used_signing_keys" int not null;');
@@ -10,6 +15,11 @@ export class Migration20231220104403 extends Migration {
   }
 
   async down(): Promise<void> {
+    this.addSql('TRUNCATE registry_key');
+    this.addSql('TRUNCATE registry_operator');
+    this.addSql('TRUNCATE el_meta_entity');
+    this.addSql('TRUNCATE sr_module_entity');
+
     this.addSql('alter table "el_meta_entity" drop column "last_changed_block_hash";');
 
     this.addSql('alter table "registry_operator" drop column "finalized_used_signing_keys";');
