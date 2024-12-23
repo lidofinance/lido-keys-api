0e81a3a78d8e0228fb7fbd69095189dad8e7c611
Anna Mukharram <amuhar3@gmail.com>
2024-12-23 16:51:50 +0400

    fix: test zero address

--- src/staking-router-modules/contracts/staking-router/staking-router-fetch.module.ts
+++ src/staking-router-modules/contracts/staking-router/staking-router-fetch.module.ts
@@ -4,15 +4,17 @@ import { StakingModuleInterfaceModule } from '../staking-module-interface';
 import { LidoLocatorModule } from '../lido-locator/lido-locator.module';
 import { StakingRouterContractModule } from '@lido-nestjs/contracts';
 import { ExecutionProvider } from '../../../common/execution-provider';
+import { ConfigService } from 'common/config';
+import { ethers } from 'ethers';
 
 @Module({
   imports: [
     StakingModuleInterfaceModule,
     LidoLocatorModule,
     StakingRouterContractModule.forRootAsync({
-      inject: [ExecutionProvider],
-      async useFactory(provider) {
-        return { provider };
+      inject: [ExecutionProvider, ConfigService],
+      async useFactory(provider, configService: ConfigService) {
+        return { provider, address: '0x0000000000000000000000000000000000000000' };
       },
     }),
   ],
