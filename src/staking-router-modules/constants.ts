9b4a9dfe2d82cc2ba05601536c3e49f96c12a569
Anna Mukharram <amuhar3@gmail.com>
2025-06-19 14:20:45 +0400

    feat: add new csm type

--- src/staking-router-modules/constants.ts
+++ src/staking-router-modules/constants.ts
@@ -1,5 +1,6 @@
 export enum STAKING_MODULE_TYPE {
   CURATED_ONCHAIN_V1_TYPE = 'curated-onchain-v1',
   COMMUNITY_ONCHAIN_V1_TYPE = 'community-onchain-v1',
+  COMMUNITY_ONCHAIN_V2_TYPE = 'community-onchain-v2',
   COMMUNITY_ONCHAIN_DEVNET0_V1_TYPE = 'community-staking-module',
 }
